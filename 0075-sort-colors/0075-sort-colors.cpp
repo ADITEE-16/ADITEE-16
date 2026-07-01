9831a989537cce9884c9049e740866931d84a3bb
ADITEE-16 <aditeetripathy@gmail.com>
2026-07-01 20:41:00 +0530

    Time: 0 ms (100%), Space: 11.9 MB (11.41%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+              count0++;
+            else if(nums[i]==1)
+             count1++;
+            else
+             count2++;
+        }
+        vector<int> arr(nums.size());
+        for(int i=0;i<count0;i++){
+            arr[i]=0;
+        }
+        for(int i=count0;i<count0+count1;i++){
+            arr[i]=1;
+        }
+        for(int i=count1+count0;i<count2+count1+count0;i++){
+            arr[i]=2;
+        }
+        nums=arr;
+    }
+};
