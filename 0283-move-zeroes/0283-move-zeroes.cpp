a9f30eab5cade7f1db922b4bd9e657b404fc9c57
ADITEE-16 <aditeetripathy@gmail.com>
2026-07-19 11:33:20 +0530

    Time: 0 ms (100%), Space: 25.2 MB (5.05%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                count++;
+            }
+            else{
+                ans.push_back(nums[i]);
+            }
+        }
+        while(count--){
+            ans.push_back(0);
+        }
+        nums=ans;
+
+    }
+};
