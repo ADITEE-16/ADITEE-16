12e58a65cd38d268b41f6df5a80339590b412844
ADITEE-16 <aditeetripathy@gmail.com>
2026-07-19 10:32:51 +0530

    Time: 71 ms (32.28%), Space: 111.3 MB (17.87%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        unordered_set<int> st;
+        for(int x : nums){
+            if(st.find(x)!=st.end()){
+                return true;
+            }
+            else{
+                st.insert(x);
+            }
+        }
+        return false;
+    }
+};
