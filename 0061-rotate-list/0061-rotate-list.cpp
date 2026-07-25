112b0c7386ca02ebf3e76affe5c38956cf5dc441
ADITEE-16 <aditeetripathy@gmail.com>
2026-07-25 17:55:37 +0530

    Time: 0 ms (100%), Space: 16.6 MB (33.17%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,32 +8,59 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* rotateRight(ListNode* head, int k) {
+//         if(head == NULL || head->next == NULL)
+//             return head;
+//         int count=0;
+//         ListNode* temp=head;
+//         while(temp!=NULL){
+//             count++;
+//             temp=temp->next;
+//         }
+//         k = k % count;
+//         if(k==0)
+//             return head;
+//         temp=head;
+//         for(int i=1;i<(count-k);i++){
+//             temp=temp->next;
+//         }
+//         ListNode* next=temp->next;
+//         ListNode* temp1=temp->next;
+//         while(temp1->next){
+//             temp1=temp1->next;
+//         }
+//         temp1->next=head;
+//         temp->next=NULL;
+//         head=next;
+//         return head;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == NULL || head->next == NULL)
             return head;
-        int count=0;
+        int count=1;
         ListNode* temp=head;
-        while(temp!=NULL){
+        while(temp->next!=NULL){
             count++;
             temp=temp->next;
         }
+        
         k = k % count;
-        if(k==0)
+        if(k == 0)
             return head;
-        temp=head;
-        for(int i=1;i<(count-k);i++){
-            temp=temp->next;
-        }
-        ListNode* next=temp->next;
-        ListNode* temp1=temp->next;
-        while(temp1->next){
-            temp1=temp1->next;
-        }
-        temp1->next=head;
-        temp->next=NULL;
-        head=next;
-        return head;
+        temp->next=head;
+        ListNode* newTail = head;
+        for(int i = 1; i < count - k; i++)
+            newTail = newTail->next;
+        ListNode* temp1=newTail->next;
+        //head=temp1;
+        newTail->next=NULL;
+        return temp1;
     }
 };
